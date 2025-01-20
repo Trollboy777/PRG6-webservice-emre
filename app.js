87456ce78e7cfb2be2e017569811180093688f1c
Patesaus77 <Emregulec70@gmail.com>
2025-01-20 10:13:32 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from "mongoose";
+import {faker} from '@faker-js/faker';
+import dotenv from 'dotenv';
+
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
